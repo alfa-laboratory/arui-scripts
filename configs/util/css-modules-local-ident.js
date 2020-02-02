65b9e58b7d767d6ace3d190cefba073e26d1708e
stepancar <stepancar@hotmail.com>
2020-02-02 03:39:30 +0300

    chore(*): use folder name for css module

--- configs/util/css-modules-local-ident.js
+++ configs/util/css-modules-local-ident.js
@@ -1,5 +1,5 @@
 function getLocalIdentPattern({ isProduction }) {
-    return '[name]__[local]__[hash:base64:5]';
+    return '[folder]__[local]__[hash:base64:5]';
 }
 
 module.exports = getLocalIdentPattern;
