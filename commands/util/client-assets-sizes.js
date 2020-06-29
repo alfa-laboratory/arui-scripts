d744fa436f193508ad078d3529f7c3a8bdfee65c
Aleksandr Kitov <akitov@alfabank.ru>
2020-06-29 10:26:20 +0300

    feat(assets-size): print brotli size when possible Fixes #114

--- commands/util/client-assets-sizes.js
+++ commands/util/client-assets-sizes.js
@@ -1,9 +1,11 @@
 const fs = require('fs');
 const path = require('path');
 const gzipSize = require('gzip-size').sync;
+const brotliSize = require('brotli-size').sync;
 const filesize = require('filesize');
 const stripAnsi = require('strip-ansi');
 const chalk = require('chalk');
+const checkNodeVersion = require('../../configs/util/check-node-version');
 
 function canReadAsset(asset) {
     return (
@@ -31,15 +33,18 @@ function calculateAssetsSizes(webpackStats, rootDir) {
                 .map(asset => {
                     const fileContents = fs.readFileSync(path.join(rootDir, asset.name));
                     const size = gzipSize(fileContents);
+                    const brSize = checkNodeVersion(10) ? brotliSize(fileContents) : NaN;
                     const filename = path.basename(asset.name);
 
                     return {
                         name: removeFileNameHash(filename),
                         fullName: filename,
                         gzipSize: size,
+                        brotliSize: brSize,
                         size: asset.size,
                         sizeLabel: filesize(asset.size),
-                        gzipLabel: filesize(size)
+                        gzipLabel: filesize(size),
+                        brotliLabel: brSize ? filesize(brSize) : '-'
                     };
                 })
         )
@@ -49,11 +54,13 @@ function calculateAssetsSizes(webpackStats, rootDir) {
         return {
             size: total.size + file.size,
             gzipSize: total.gzipSize + file.gzipSize,
+            brotliSize: total.brotliSize + file.brotliSize
         };
-    }, { size: 0, gzipSize: 0 });
+    }, { size: 0, gzipSize: 0, brotliSize: 0 });
 
     totalSizes.sizeLabel = filesize(totalSizes.size);
     totalSizes.gzipLabel = filesize(totalSizes.gzipSize);
+    totalSizes.brotliLabel = totalSizes.brotliSize ? filesize(totalSizes.brotliSize) : '-';
 
     return {
         totalSizes,
@@ -70,7 +77,7 @@ function printAssetsSizes(sizes) {
     console.log(chalk.blueBright('Assets sizes:'));
 
     sizes.assets.forEach(asset => {
-        let sizeLabel = `${asset.sizeLabel} (${asset.gzipLabel} gzip)`;
+        let sizeLabel = `${asset.sizeLabel} (${asset.gzipLabel} gzip, ${asset.brotliLabel} br)`;
         const sizeLength = stripAnsi(sizeLabel).length;
 
         if (sizeLength < longestSizeLabelLength) {
@@ -88,7 +95,7 @@ function printAssetsSizes(sizes) {
 
     console.log(
         chalk.blueBright('\nTotal size:\n') +
-        `  ${sizes.totalSizes.sizeLabel} (${sizes.totalSizes.gzipLabel} gzip)\n`
+        `  ${sizes.totalSizes.sizeLabel} (${sizes.totalSizes.gzipLabel} gzip, ${sizes.totalSizes.brotliLabel} br)\n`
     );
 }
 
