8ef5c70d726d5f1e8a2fedaf06d5e2950198f7dd
doyakovlev <doyakovlev@alfabank.ru>
2021-02-21 22:41:32 +0300

    chore(webpack.client-dev): add hmr

--- packages/arui-scripts/src/configs/webpack.client.dev.ts
+++ packages/arui-scripts/src/configs/webpack.client.dev.ts
@@ -252,7 +252,7 @@ const webpackClientDev = applyOverrides<webpack.Configuration>(['webpack', 'webp
             'process.env': '{}' 
         }),
         // This is necessary to emit hot updates (currently CSS only):
-        // new webpack.HotModuleReplacementPlugin(),
+        new webpack.HotModuleReplacementPlugin(),
         // Watcher doesn't work well if you mistype casing in a path so we use
         // a plugin that prints an error when you attempt to do this.
         // See https://github.com/facebookincubator/create-react-app/issues/240
