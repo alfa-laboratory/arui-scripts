b3a8bf48d2d234e369756b2476decf3629bafe2c
doyakovlev <doyakovlev@alfabank.ru>
2021-02-24 14:48:44 +0300

    feat(webpack.client.dev): add webpack-dev-server to entry

--- packages/arui-scripts/src/configs/webpack.client.dev.ts
+++ packages/arui-scripts/src/configs/webpack.client.dev.ts
@@ -24,7 +24,7 @@ function getSingleEntry(clientEntry: string[]) {
     return [
         configs.clientPolyfillsEntry,
         require.resolve('react-hot-loader/patch'),
-        // `${require.resolve('webpack-dev-server/client')}?/`,
+        `${require.resolve('webpack-dev-server/client/default')}?/`,
         require.resolve('webpack/hot/dev-server'),
         // Finally, this is your app's code:
         ...clientEntry,
