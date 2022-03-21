18e22be27e852766af2a4120007cb25810f20279
iishabakaev <iishabakaev@gmail.com>
2022-03-21 15:37:33 +0300

    feat(dockerfile.template.ts): optimize Dockerfile

--- packages/arui-scripts/src/templates/dockerfile.template.ts
+++ packages/arui-scripts/src/templates/dockerfile.template.ts
@@ -1,9 +1,9 @@
 import configs from '../configs/app-configs';
 import applyOverrides from '../configs/util/apply-overrides';
 
+const chownForAdd = configs.runFromNonRootUser ? '--chown=nginx:nginx' : ''
 const nginxNonRootPart = configs.runFromNonRootUser ?
-  `RUN chown -R nginx:nginx /src && chmod -R 755 /src && \\
-       mkdir -p /var/lib/nginx && \\
+  `RUN mkdir -p /var/lib/nginx && \\
        chown -R nginx:nginx /var/lib/nginx && \\
        chown -R nginx:nginx /var/log/nginx && \\
        chown -R nginx:nginx /etc/nginx/conf.d
@@ -19,12 +19,12 @@ FROM ${configs.baseDockerImage}
 ARG START_SH_LOCATION
 ARG NGINX_CONF_LOCATION
 
-WORKDIR /src
-ADD $START_SH_LOCATION /src/start.sh
-ADD $NGINX_CONF_LOCATION /src/nginx.conf
-ADD . /src
-
 ${nginxNonRootPart}
+
+WORKDIR /src
+ADD ${chownForAdd} $START_SH_LOCATION /src/start.sh
+ADD ${chownForAdd} $NGINX_CONF_LOCATION /src/nginx.conf
+ADD ${chownForAdd} . /src
 `;
 
 export default applyOverrides('Dockerfile', dockerfileTemplate);
